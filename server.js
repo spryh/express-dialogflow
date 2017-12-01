e7aaf331fe802cae05b51df7554803528b32ef91
Paul Scarpa <spryh@Bootstrap.home>
2017-11-30 18:21:11 -0800

    thru 7.33 Subtract, Mult and Div

--- server.js
+++ server.js
@@ -1,34 +1,70 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+var express = require("express");
+var bodyParser = require("body-parser");
 
-var app = express()
+var app = express();
 
-var port = process.env.PORT || 3000
-var ip = process.env.IP || '127.0.0.1'
+var port = process.env.PORT || 3000;
+var ip = process.env.IP || "127.0.0.1";
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.post('/', function (req, res) {
-  console.log(req.body)
+app.post("/", function(req, res) {
+  console.log(req.body);
+  var response = "";
 
-  if (req.body.result.action == 'checkVote') {
-    var age = req.body.result.parameters.age
-    console.log(age)
-
-    var response = ''
+  if (req.body.result.action == "checkVote") {
+    var age = req.body.result.parameters.age;
+    console.log(age);
 
     if (age.amount >= 18) {
-      response = 'Yes'
+      response = "Yes";
     } else {
-      response = 'No'
+      response = "No";
     }
+  } 
+  
+  else if (req.body.result.action == "Add") {
+    var num1 = parseFloat(req.body.result.parameters.number1);
+    var num2 = parseFloat(req.body.result.parameters.number2);
 
-    res.json({
-      speech: response,
-      displayText: response
-    })
+    var sum = num1 + num2;
+
+    var response = `The sum of ${num1} and ${num2} is ${sum}`;
   }
-})
 
-app.listen(port, ip)
+  else if (req.body.result.action == "Subtract") {
+    var num1 = parseFloat(req.body.result.parameters.number1);
+    var num2 = parseFloat(req.body.result.parameters.number2);
+
+    var diff = num1 - num2;
+
+    var response = `The difference of ${num1} and ${num2} is ${diff}`;
+  }
+
+  else if (req.body.result.action == "Mult") {
+    var num1 = parseFloat(req.body.result.parameters.number1);
+    var num2 = parseFloat(req.body.result.parameters.number2);
+
+    var mult = num1 - num2;
+
+    var response = `The difference of ${num1} and ${num2} is ${mult}`;
+  }
+
+  else if (req.body.result.action == "Div") {
+    var num1 = parseFloat(req.body.result.parameters.number1);
+    var num2 = parseFloat(req.body.result.parameters.number2);
+
+    var div = num1 - num2;
+
+    var response = `The difference of ${num1} and ${num2} is ${div}`;
+  }
+
+  res.json({
+    speech: response,
+    displayText: response
+  })
+
+});
+
+app.listen(port, ip);
