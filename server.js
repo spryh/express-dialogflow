457c6b3ba40f2cb4f786d9b210fb0b927d20745f
Paul Scarpa <spryh@Bootstrap.home>
2017-12-01 19:02:53 -0800

    thru 538b - URI encoding and clean up

--- server.js
+++ server.js
@@ -14,80 +14,79 @@ app.post("/", function(req, res) {
   var response = "";
 
   if (req.body.result.action == "checkVote") {
-    console.log("*******Vote*******")
-    var age = req.body.result.parameters.age;
-    console.log(age);
-
-  //   if (age.amount >= 18) {
-  //     response = "Yes";
-  //   } else {
-  //     response = "No";
-  //   }
-  } else if (req.body.result.action == "Add" ) {
-    console.log("*******Math*******")
-  //   var num1 = parseFloat(req.body.result.parameters.number1);|| "Subtract" || "Mult" || "Div"
-  //   var num2 = parseFloat(req.body.result.parameters.number2);
-
-  //   if (req.body.result.action == "Add") {
-  //     var sum = num1 + num2;
-  //     var response = `The sum of ${num1} and ${num2} is ${sum}`;
-  //   } else if (req.body.result.action == "Subtract") {
-  //     var diff = num1 - num2;
-  //     var response = `The difference of ${num1} and ${num2} is ${diff}`;
-  //   } else if (req.body.result.action == "Mult") {
-  //     var mult = num1 * num2;
-  //     var response = `The product of ${num1} and ${num2} is ${mult}`;
-  //   } else if (req.body.result.action == "Div") {
-  //     var div = num1 / num2;
-  //     var response = `${num1} divided by ${num2} is ${div}`;
-  //   }
-  } else if (req.body.result.action == "getWeather" ) {
-    console.log("*******Weather*******")
-    var city = encodeURI(req.body.result.parameters.city);
-    const APP_ID = "284662c8a7d8adc1b60123a3b3dd95d0";
-    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APP_ID}`;
-
-    //console.log(url)
-    // request(url, function(error, result, body) {
-    //   console.log('TEMP:', JSON.parse(body).main.temp)   
-    //   var temp = Math.round(JSON.parse(body).main.temp - 273.15);
-    //   console.log('C TEMP:',temp)  
-    //   var response = `The temperature in ${city} is ${temp} degrees celcius.`;
-    //   console.log('Response:',response)  
-    // });
-    var request = {
-      uri: "http://api.openweathermap.org/data/2.5/weather?",
-      qs: {
-        'q': city,
-        'appid': APP_ID
-      },
-      headers: {
-          'User-Agent': 'Request-Promise'
-      },
-      json: true // Automatically parses the JSON string in the response
-  };
-    requestPromise(request)
-    .then(response => {
-        console.log(response.main.temp);
-        var temp = Math.round(response.main.temp - 273.15);
-        var answer = `The temperature in ${city} is ${temp} degrees celcius.`;
-        console.log(answer)   
-    })
-    .catch(err => {
-        // API call failed...
+    console.log("*******Vote*******");
+    var age = req.body.result.parameters.age.amount;
+    if (age >= 18) {
+      response = `Yes, you can vote at ${age}`;
+    } else {
+      response = `No, you can't vote at ${age}`;
+    }
+    console.log(response);
+    res.json({
+      speech: response,
+      displayText: response
     });
 
-  } else {
-    console.log("*******OTHER*******")
-  }
+  } else if (
+    req.body.result.action in { Add: "", Subtract: "", Mult: "", Div: "" }) {
+    console.log("*******Math*******");
+    var num1 = parseFloat(req.body.result.parameters.number1);
+    var num2 = parseFloat(req.body.result.parameters.number2);
 
-  //console.log("******* JSON PAYLOAD *******/n",response)
-  
-  res.json({
-    speech: response,
-    displayText: response
-  });
-  //console.log(req.body);
+    if (req.body.result.action == "Add") {
+      var sum = num1 + num2;
+      var response = `The sum of ${num1} and ${num2} is ${sum}`;
+    } else if (req.body.result.action == "Subtract") {
+      var diff = num1 - num2;
+      var response = `The difference of ${num1} and ${num2} is ${diff}`;
+    } else if (req.body.result.action == "Mult") {
+      var mult = num1 * num2;
+      var response = `The product of ${num1} and ${num2} is ${mult}`;
+    } else if (req.body.result.action == "Div") {
+      var div = num1 / num2;
+      var response = `${num1} divided by ${num2} is ${div}`;
+    }
+    console.log(response);
+    res.json({
+      speech: response,
+      displayText: response
+    });
+
+  } else if (req.body.result.action == "getWeather") {
+    console.log("*******Weather*******");
+    var city = encodeURIComponent(req.body.result.parameters.city);
+    const APP_ID = "284662c8a7d8adc1b60123a3b3dd95d0";
+
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APP_ID}`;
+    console.log(url)
+
+    var request = {
+      uri: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APP_ID}`,
+      // qs: {
+      //   q: "" + city,
+      //   appid: APP_ID
+      // },
+      headers: {"User-Agent": "Request-Promise"},
+      json: true
+    };
+
+    requestPromise(request)
+      .then(responseJSON => {
+        console.log(JSON.stringify(responseJSON.name));
+        var temp = Math.round(((9/5)*(responseJSON.main.temp - 273.15)) + 32);
+        response = `The temperature in ${decodeURI(city)} is ${temp} degrees farenheit.`;
+        console.log(response);
+        res.json({
+          speech: response,
+          displayText: response
+        });
+      })
+      .catch(err => {
+        // API call failed...
+      });
+  } else {
+    console.log("*******OTHER*******");
+  }
 });
 
 app.listen(port, ip);
